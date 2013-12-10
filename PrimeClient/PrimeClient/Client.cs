0b2a2ac815596da23a862e6071d4a6c0f67a1edb
Seter17 <seter17@gmail.com>
2013-12-11 02:35:17 +0400

    git commit bugs

---
+++ PrimeClient/PrimeClient/Client.cs
@@ -0,0 +1,300 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using PrimeLibrary;
+using PrimeServer;
+
+namespace PrimeClient
+{
+    class Client
+    {
+        enum ConnectType
+        {
+            Request,
+            Store
+        }
+
+        #region Fields
+
+        private static volatile Client instance;
+        private static readonly object lockObject = new object();
+
+        private bool firstRequest = true;
+
+        private const int port = 11000;
+
+        private Socket client;
+
+        private Guid id;
+        private List<ulong> primes;
+
+	    #endregion
+
+        private Client()
+        {
+            primes = new List<ulong>();
+        }
+
+        #region Event & Handlers
+        public static event EventHandler<string> Connected;
+        private void ConnectedEventHandler()
+        {
+            if (Connected == null) return;
+            Connected.Invoke(this, "Connection established");
+        }
+
+        public static event EventHandler<string> DataRecieved;
+        private void DataRecievedEventHandler(string data)
+        {
+            if (DataRecieved == null) return;
+            DataRecieved.Invoke(this, data);
+        }
+
+        public static event EventHandler<string> Sent;
+        private void SentEventHandler(string s)
+        {
+            if (Sent == null) return;
+            Sent.Invoke(this, s);
+        }
+		
+        public static event EventHandler<Exception> Exception;
+        private void ExceptionEventHandler(Exception e)
+        {
+            if (Exception == null) return;
+            Exception.Invoke(this, e);
+        }
+
+	    #endregion
+
+        #region Control & Access
+
+        public static Client Instance
+        {
+            get
+            {
+                if (instance != null) return instance;
+                lock (lockObject)
+                {
+                    if(instance == null)
+                        instance = new Client();
+                }
+                return instance;
+            }
+        }
+
+        #region Connection methods
+
+        public void RequestNumbers()
+        {
+            Connect(ConnectType.Request);
+        }
+
+        public void StorePrimes()
+        {
+            Connect(ConnectType.Store);
+        }
+
+        public void Disconnect()
+        {
+            if (client == null || !client.Connected) return;
+
+            client.Shutdown(SocketShutdown.Both);
+            client.Close();
+        }
+
+        private void Connect(ConnectType type)
+        {
+            Disconnect();
+
+            try
+            {
+                // Establish the remote endpoint for the socket.
+                IPHostEntry ipHostInfo = Dns.Resolve("localhost");
+                IPAddress ipAddress = ipHostInfo.AddressList[0];
+                IPEndPoint remoteEP = new IPEndPoint(ipAddress, port);
+
+                // Create a TCP/IP socket.
+                client = new Socket(AddressFamily.InterNetwork,
+                    SocketType.Stream, ProtocolType.IP);
+
+                // Connect to the remote endpoint.
+                client.BeginConnect(remoteEP, ConnectCallback, type);
+            }
+            catch (Exception e)
+            {
+                ExceptionEventHandler(e);
+            }
+        }
+        
+        #endregion
+
+        #region Send methods
+
+        private void Send(Socket client, String data)
+        {
+            // Convert the string data to byte data using ASCII encoding.
+            var byteData = Encoding.ASCII.GetBytes(data);
+
+            var state = new StateObject { workSocket = client };
+            state.sb.Append(data);
+            // Begin sending the data to the remote device.
+            client.BeginSend(byteData, 0, byteData.Length, 0, SendCallback, state);
+        }
+
+        private void SendStoreMessage()
+        {
+            var message = String.Format("Gotcha {0} {1}<EOF>", id, String.Join(",", primes));
+            Send(client, message);
+        }
+
+        private void SendRequestMessage()
+        {
+            //prepare for the next iteration
+            primes.Clear();
+            Send(client, "Gimme<EOF>");
+        }
+        
+        #endregion
+
+        private void Receive(Socket client)
+        {
+            try
+            {
+                // Create the state object.
+                var state = new StateObject();
+                state.workSocket = client;
+
+                // Begin receiving the data from the remote device.
+                client.BeginReceive(state.buffer, 0, StateObject.BufferSize, 0,
+                    ReceiveCallback, state);
+            }
+            catch (Exception e)
+            {
+                
+            }
+        }
+
+        private void ParseMessage(string message)
+        {
+            if (message.Equals("Thanks!"))
+            {
+                RequestNumbers();
+                return;
+            }
+
+            var data = message.Split(' ');
+            //poor check, I know, better to have id for every message but this demands time and since we have only one message incomimg so we can skip it for later
+            if (data.Length != 3) return;
+
+            try
+            {
+                id = Guid.Parse(data[0]);
+                var startValue = UInt64.Parse(data[1]);
+                var range = Int32.Parse(data[2]);
+
+                PrimeChecker.CheckRange(startValue, range, ref primes);
+
+                StorePrimes();
+            }
+            catch (Exception ex)
+            {
+                ExceptionEventHandler(ex);
+            }
+        }
+
+	    #endregion
+
+        #region Callbacks
+        
+        private void ConnectCallback(IAsyncResult ar)
+        {
+            try
+            {
+                // Complete the connection.
+                client.EndConnect(ar);
+
+                ConnectedEventHandler();
+
+                var type = (ConnectType)ar.AsyncState;
+                switch (type)
+                {
+                    case  ConnectType.Request:
+                        SendRequestMessage();
+                        break;
+                    case ConnectType.Store:
+                        SendStoreMessage();
+                        break;
+                }
+            }
+            catch (Exception e)
+            {
+                ExceptionEventHandler(e);
+            }
+        }
+
+        private  void ReceiveCallback(IAsyncResult ar)
+        {
+            try
+            {
+                // Retrieve the state object and the client socket 
+                // from the asynchronous state object.
+                var state = (StateObject)ar.AsyncState;
+                var socket = state.workSocket;
+
+                // Read data from the remote device.
+                var bytesRead = socket.EndReceive(ar);
+
+                if (bytesRead > 0)
+                {
+                    // There might be more data, so store the data received so far.
+                    state.sb.Append(Encoding.ASCII.GetString(state.buffer, 0, bytesRead));
+
+                    // Get the rest of the data.
+                    client.BeginReceive(state.buffer, 0, StateObject.BufferSize, 0,
+                        ReceiveCallback, state);
+                }
+                else
+                {
+                    // All the data has arrived
+                    if (state.sb.Length <= 1) return;
+
+                    Disconnect();
+
+                    DataRecievedEventHandler(state.sb.ToString().EraseEnding());
+
+                    ParseMessage(state.sb.ToString().EraseEnding());
+                }
+            }
+            catch (Exception e)
+            {
+                ExceptionEventHandler(e);
+            }
+        }
+
+        private  void SendCallback(IAsyncResult ar)
+        {
+            try
+            {
+                var state = (StateObject)ar.AsyncState;
+
+                state.workSocket.EndSend(ar);
+
+                SentEventHandler(state.sb.ToString().EraseEnding());
+
+                Receive(state.workSocket);
+
+            }
+            catch (Exception e)
+            {
+               ExceptionEventHandler(e);
+            }
+        }
+
+        #endregion
+
+    }
+}
---
+++ PrimeServer/PrimeServer/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeServer
+{
+    class Program
+    {
+        private static bool isclosing = false;
+
+        enum MessageType
+        {
+            Notification,
+            Warning,
+            Exception,
+            Common
+        }
+
+        static void Main(string[] args)
+        {
+            SetConsoleCtrlHandler(new HandlerRoutine(ConsoleCtrlCheck), true);
+
+            Server.Started += (sender, s) => WriteToSystemLog(s, MessageType.Notification);
+            //Server.Listening += (sender, s) => this.WriteToSystemLog(s, MessageType.Notification);
+            Server.DataRecieved += (sender, s) =>
+                                   {
+                                       if (s.Equals("Gimme")) return; //we do not need this shit
+                                       WriteToSystemLog(String.Format("Recieved Message: {0}", s),
+                                           MessageType.Common);
+                                   };
+            Server.Sent += (sender, s) =>
+                WriteToSystemLog(String.Format("Sent Message: {0}", s), MessageType.Notification);
+
+            Server.TimeoutCheckBegan += (sender, ar) =>
+                WriteToSystemLog("Checking pending values...", MessageType.Warning);
+
+            Server.Exception += (sender, e) => WriteToSystemLog(e.Message, MessageType.Exception);
+
+            Server.Instance.Launch();
+
+            var key  = Console.ReadKey();
+            if (key.Key == ConsoleKey.Escape)
+            {
+                Server.Instance.Stop();
+            }
+        }
+
+        static private void WriteToSystemLog(string message, MessageType type)
+        {
+            switch (type)
+            {
+                case MessageType.Notification:
+                    Console.ForegroundColor = ConsoleColor.DarkGreen;
+                    break;
+                case MessageType.Warning:
+                    Console.ForegroundColor = ConsoleColor.DarkYellow;
+                    break;
+                case MessageType.Exception:
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    break;
+                default:
+                    Console.ForegroundColor = ConsoleColor.White;
+                    break;
+            }
+
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+
+        private static bool ConsoleCtrlCheck(CtrlTypes ctrlType)
+        {
+
+            switch (ctrlType)
+            {
+                case CtrlTypes.CTRL_CLOSE_EVENT:
+                    WriteToSystemLog("Program being closed!",MessageType.Warning);
+                    Server.Instance.Stop();
+                    break;
+            }
+
+            return true;
+        }
+
+        #region unmanaged
+        // Declare the SetConsoleCtrlHandler function
+        // as external and receiving a delegate.
+        [DllImport("Kernel32")]
+        public static extern bool SetConsoleCtrlHandler(HandlerRoutine Handler, bool Add);
+
+        // A delegate type to be used as the handler routine
+        // for SetConsoleCtrlHandler.
+        public delegate bool HandlerRoutine(CtrlTypes CtrlType);
+
+        // An enumerated type for the control messages
+        // sent to the handler routine.
+        public enum CtrlTypes
+        {
+            CTRL_C_EVENT = 0,
+            CTRL_BREAK_EVENT,
+            CTRL_CLOSE_EVENT,
+            CTRL_LOGOFF_EVENT = 5,
+            CTRL_SHUTDOWN_EVENT
+        }
+
+        #endregion
+    }
+}
